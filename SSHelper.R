f7282517bbf94e1f16fbc9acfdbb41b155a3b16e
fndanu <fndanu@Faiths-MacBook.local>
2023-12-06 19:42:33 -0500

    revised to return list of j_titles

--- SSHelper.R
+++ SSHelper.R
@@ -1,5 +1,7 @@
 library(httr)
 library(rvest)
+library(purrr)
+
 
 
 
@@ -7,7 +9,7 @@ search_terms = readline(prompt = "What is the title of your request?: ")
 category = readline(prompt = "I your request a books, articles, rare_books, video, journals or special_collections?: ")
 
 
-library_search <- function(search_terms, category...){
+library_search <- function(search_terms, category, ...){
 
   search_terms  <- as.character(search_terms)
 
@@ -22,10 +24,12 @@ book_title<- function(title,...){
     stop("Input wasn't a character. Please try again")
   }
 
+
   user_input = title
 
 
   place_to_search <- "journals"
+
 
   URL <- paste0("https://libtools.smith.edu/bento/results.php?",
                 "mat=", place_to_search,
@@ -33,35 +37,52 @@ book_title<- function(title,...){
                 "query=", search_terms
   )
 
-  URL_orig <- "https://libtools.smith.edu/bento/results.php?mat=journals&smith=&peer_reviewed=&full_text=&sort=&fieldcode=&perpage=6&query=1984"
 
   x <- jsonlite::read_json(URL)
 
-  str(x)
+ # str(x)
 
+ docs = map_chr(x$docs, "j_title")
+
+ # for (j in length(docs)){
+ #   num = unlist(docs[[j]])
+ #
+ #   num[[1]]
+ #
+ #   if(num[[1]] == "j_title" ){
+ #
+ #   }
+ # }
+
+ return(docs)
 
 }
 
 
+search_terms = readline(prompt = "What is the title of your request?: ")
+category = readline(prompt = "I your request a books, articles, rare_books, video, journals or special_collections?: ")
 
 
+library_search(search_terms, category)
 
-search_terms <- "fear+and+loathing+in+las+vegas"
 
-# This could be "books", "articles", "rare_books", "video", "journals" or "special_collections"
-place_to_search <- "journals"
-
-URL <- paste0("https://libtools.smith.edu/bento/results.php?",
-              "mat=", place_to_search,
-              "&smith=&peer_reviewed=&full_text=&sort=&fieldcode=&perpage=6&",
-              "query=", search_terms
-)
-
-URL_orig <- "https://libtools.smith.edu/bento/results.php?mat=journals&smith=&peer_reviewed=&full_text=&sort=&fieldcode=&perpage=6&query=1984"
-
-x <- jsonlite::read_json(URL)
-
-str(x)
+#
+# search_terms <- "fear+and+loathing+in+las+vegas"
+#
+# # This could be "books", "articles", "rare_books", "video", "journals" or "special_collections"
+# place_to_search <- "journals"
+#
+# URL <- paste0("https://libtools.smith.edu/bento/results.php?",
+#               "mat=", place_to_search,
+#               "&smith=&peer_reviewed=&full_text=&sort=&fieldcode=&perpage=6&",
+#               "query=", search_terms
+# )
+#
+# URL_orig <- "https://libtools.smith.edu/bento/results.php?mat=journals&smith=&peer_reviewed=&full_text=&sort=&fieldcode=&perpage=6&query=1984"
+#
+# x <- jsonlite::read_json(URL)
+#
+# str(x)
 
 
   query_string <- paste0("https://www.smith.edu/libraries/discover/search.php?fieldcode=&query=",

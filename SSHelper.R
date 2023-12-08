94f3ba808c691b360eddcc78db4ea839d33df389
fndanu <fndanu@Faiths-MacBook.local>
2023-12-08 14:31:31 -0500

    revised function

--- SSHelper.R
+++ SSHelper.R
@@ -2,31 +2,124 @@ library(httr)
 library(rvest)
 
 
-book_title<- function(title,...){
+library(httr)
+library(rvest)
+library(purrr)
 
-  title <- as.character(title)
 
-  if(is.na(title)){
+
+library_search <- function(search_terms, category, ...){
+
+  search_terms  <- as.character(search_terms)
+
+  if(is.na(search_terms)){
     stop("Input wasn't a character. Please try again")
   }
 
-  user_input = title
-
-
-  query_string <- paste0("https://www.smith.edu/libraries/discover/search.php?fieldcode=&query=",
-                         user_input,
-                         "&box=books&box=articles&box=rare_books&box=video&box=journals&box=special_collections"
+  URL <- paste0("https://libtools.smith.edu/bento/results.php?",
+                "mat=", place_to_search,
+                "&smith=&peer_reviewed=&full_text=&sort=&fieldcode=&perpage=6&",
+                "query=", search_terms
   )
-  x <- httr::GET(query_string)
 
-  library_section <- rvest::read_html(x$content) |>
-    html_element(css = ".col-md-4")
 
-  library_section |>
-    html_elements(".btitle")
+  x <- jsonlite::read_json(URL)
 
-  return(library_section)
+  # str(x)
+
+  docs = map_chr(x$docs, "j_title")
+
+  # for (j in length(docs)){
+  #   num = unlist(docs[[j]])
+  #
+  #   num[[1]]
+  #
+  #   if(num[[1]] == "j_title" ){
+  #
+  #   }
+  # }
+
+  return(docs)
+
 }
+
+
+search_terms = readline(prompt = "What is the title of your request?: ")
+category = readline(prompt = "I your request a books, articles, rare_books, video, journals or special_collections?: ")
+
+
+library_search(search_terms, category)
+
+
+#
+# search_terms <- "fear+and+loathing+in+las+vegas"
+#
+# # This could be "books", "articles", "rare_books", "video", "journals" or "special_collections"
+# place_to_search <- "journals"
+#
+# URL <- paste0("https://libtools.smith.edu/bento/results.php?",
+#               "mat=", place_to_search,
+#               "&smith=&peer_reviewed=&full_text=&sort=&fieldcode=&perpage=6&",
+#               "query=", search_terms
+# )
+#
+# URL_orig <- "https://libtools.smith.edu/bento/results.php?mat=journals&smith=&peer_reviewed=&full_text=&sort=&fieldcode=&perpage=6&query=1984"
+#
+# x <- jsonlite::read_json(URL)
+#
+# str(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# book_title<- function(title,...){
+#
+#   title <- as.character(title)
+#
+#   if(is.na(title)){
+#     stop("Input wasn't a character. Please try again")
+#   }
+#
+#   user_input = title
+#
+#
+#   query_string <- paste0("https://www.smith.edu/libraries/discover/search.php?fieldcode=&query=",
+#                          user_input,
+#                          "&box=books&box=articles&box=rare_books&box=video&box=journals&box=special_collections"
+#   )
+#   x <- httr::GET(query_string)
+#
+#   library_section <- rvest::read_html(x$content) |>
+#     html_element(css = ".col-md-4")
+#
+#   library_section |>
+#     html_elements(".btitle")
+#
+#   return(library_section)
+# }
 
 
 

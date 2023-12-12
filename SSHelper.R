c3dd6cb708df333d516f64457d15f0abdc0dd144
SaliemaG <sghebreghiorgis@smith.edu>
2023-12-12 13:12:15 -0500

    Cover images

--- SSHelper.R
+++ SSHelper.R
@@ -1,31 +1,26 @@
 library(httr)
 library(rvest)
+library(purrr)
+library(gridExtra)
+library(grid)
 
 
+library_search <- function(search_terms, place_to_search, ...){
 
-search_terms = readline(prompt = "What is the title of your request?: ")
-category = readline(prompt = "I your request a books, articles, rare_books, video, journals or special_collections?: ")
+  place_to_search <- as.character(place_to_search)
 
+  category_list <- list("books", "articles", "rare_books", "video", "journals", "special_collections")
 
-library_search <- function(search_terms, category...){
+  if(!(place_to_search%in% category_list)){
+    stop("The provided category is not available")
+  }
+
 
   search_terms  <- as.character(search_terms)
 
   if(is.na(search_terms)){
-
-book_title<- function(title,...){
-
-  title <- as.character(title)
-
-  if(is.na(title)){
-
     stop("Input wasn't a character. Please try again")
   }
-
-  user_input = title
-
-
-  place_to_search <- "journals"
 
   URL <- paste0("https://libtools.smith.edu/bento/results.php?",
                 "mat=", place_to_search,
@@ -33,60 +28,115 @@ book_title<- function(title,...){
                 "query=", search_terms
   )
 
-  URL_orig <- "https://libtools.smith.edu/bento/results.php?mat=journals&smith=&peer_reviewed=&full_text=&sort=&fieldcode=&perpage=6&query=1984"
-
   x <- jsonlite::read_json(URL)
+  str( x)
 
-  str(x)
+
+
+
+  if ( place_to_search == "journals"){
+    cover_images <- map_chr(x$docs, "coverimage")
+    journals = map_chr(x$docs, "j_title")
+    names(cover_images) <- journals
+    journals
+
+    draw_image(cover_images)
+
+  } else if ( place_to_search == "books"){
+    books = map_chr(x$e.fivecolleges.fol., "jtitle")
+    books
+    # cover_image = map_chr(x$, "")
+
+    # }else if (category == "articles"){
+    #   # artcles = map_chr(x)
+
+  }else if (place_to_search == "rare_books"){
+    rare_books = map_chr(x$e.fivecolleges.fol., "full")
+    rare_books
+
+  }else if (place_to_search == "video"){
+    video = map_chr(x$e.fivecolleges.fol., "full ")
+    video
+
+  }else if( place_to_search == "special_collections"){
+    special_collections = map_chr(x$ries.2.resources., "full")
+    special_collections
+
+  } else{
+
+    stop("Category not found")
+  }
+
+
 
 
 }
 
+draw_image<- function(cover_images,...){
 
+  n <- length(cover_images)
+  n_row <- floor(sqrt(n))
+  n_col <- ceiling(n / n_row)
 
+  # Set up the plot layout
+  par(mfrow = c(n_row, n_col))
 
+  for (j in cover_images){
 
-search_terms <- "fear+and+loathing+in+las+vegas"
+    img_type <- tools::file_ext(j)
 
-# This could be "books", "articles", "rare_books", "video", "journals" or "special_collections"
-place_to_search <- "journals"
+    # tmp <- tempfile(pattern = "file",
+    #                 tmpdir = tempdir()
+    #   )
+    #
+    # utils::download.file(cover_images, destfile = tmp)
 
-URL <- paste0("https://libtools.smith.edu/bento/results.php?",
-              "mat=", place_to_search,
-              "&smith=&peer_reviewed=&full_text=&sort=&fieldcode=&perpage=6&",
-              "query=", search_terms
-)
+    tmp <- httr::GET(url = j)
 
-URL_orig <- "https://libtools.smith.edu/bento/results.php?mat=journals&smith=&peer_reviewed=&full_text=&sort=&fieldcode=&perpage=6&query=1984"
+    if (img_type == "png") {
+      p <- png::readPNG(tmp$content)
+    } else if (img_type == "jpg" || img_type == "jpeg") {
+      p <- jpeg::readJPEG(tmp$content)
+    } else {
+      stop("unknown image format", img_type)
+    }
+    graphics::plot.new()
+    grid::grid.raster(p)
 
-x <- jsonlite::read_json(URL)
+  }
 
-str(x)
-
-
-  query_string <- paste0("https://www.smith.edu/libraries/discover/search.php?fieldcode=&query=",
-                         user_input,
-                         "&box=books&box=articles&box=rare_books&box=video&box=journals&box=special_collections"
-  )
-  x <- httr::GET(query_string)
-
-  library_section <- rvest::read_html(x$content) |>
-    html_element(css = ".col-md-4")
-
-  library_section |>
-    html_elements(".btitle")
-
-  return(library_section)
 }
 
 
+# search_terms = readline(prompt = "What is the title of your request?: ")
+# category = readline(prompt = "I your request a books, articles, rare_books, video, journals or special_collections?: ")
+
+
+
+#
+# search_terms <- "fear+and+loathing+in+las+vegas"
+#
+# # This could be "books", "articles", "rare_books", "video", "journals" or "special_collections"
+# place_to_search <- "journals"
+#
+# URL <- paste0("https://libtools.smith.edu/bento/results.php?",
+#               "mat=", place_to_search,
+#               "&smith=&peer_reviewed=&full_text=&sort=&fieldcode=&perpage=6&",
+#               "query=", search_terms
+# )
+#
+# URL_orig <- "https://libtools.smith.edu/bento/results.php?mat=journals&smith=&peer_reviewed=&full_text=&sort=&fieldcode=&perpage=6&query=1984"
+#
+# x <- jsonlite::read_json(URL)
+#
+# str(x)
 
 
 
 
 
 
-
+# grid graphics subplots--> to show all images
 
 
 

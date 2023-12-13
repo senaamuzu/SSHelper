3f444dd638a0ec3aac340ee8f9b7371fcfc31d56
senaamuzu <samuzu@smith.edu>
2023-12-13 10:32:09 -0500

    remove dependency on magick library

--- R/SSHelper.R
+++ R/SSHelper.R
@@ -3,7 +3,6 @@ library(rvest)
 library(purrr)
 library(gridExtra)
 library(grid)
-library(magick)
 
 #' @title Retrieve metadata from Smith College library
 #'
@@ -146,47 +145,3 @@ draw_image<- function(cover_images,...){
   }
 
 }
-
-
-# magick::image_read
-#
-# jpeg::readJPEG
-
-# search_terms = readline(prompt = "What is the title of your request?: ")
-# category = readline(prompt = "I your request a books, articles, rare_books, video, journals or special_collections?: ")
-
-
-
-#
-# search_terms <- "fear+and+loathing+in+las+vegas"
-#
-# # This could be "books", "articles", "rare_books", "video", "journals" or "special_collections"
-# place_to_search <- "journals"
-#
-# URL <- paste0("https://libtools.smith.edu/bento/results.php?",
-#               "mat=", place_to_search,
-#               "&smith=&peer_reviewed=&full_text=&sort=&fieldcode=&perpage=6&",
-#               "query=", search_terms
-# )
-#
-# URL_orig <- "https://libtools.smith.edu/bento/results.php?mat=journals&smith=&peer_reviewed=&full_text=&sort=&fieldcode=&perpage=6&query=1984"
-#
-# x <- jsonlite::read_json(URL)
-#
-# str(x)
-
-
-
-
-
-
-# grid graphics subplots--> to show all images
-
-
-
-
-
-
-
-
-
